fb253c0f192507bb9a57fd70beeb9054ee2cfded
ducpmph34277 <ducpmph34277@fpt.edu.vn>
2025-06-15 23:57:30 +0700

    Thêm giao diện default

---
+++ move-up-web-fe/src/client/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="bg-light text-center text-muted py-3 border-top mt-5">
+    <div class="container">
+      <small>&copy; 2025 ShopEasy. All rights reserved.</small>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+  font-size: 0.9rem;
+}
+</style>
